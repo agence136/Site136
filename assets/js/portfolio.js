63c2a812e74867e2db18e34c576ac68983b6e38b
agence136 <elisabeth.malek@agence136.fr>
2023-08-07 13:23:16 +0200

    Ajout de travaux dans le fichier json production

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,48 +1,6 @@
 let portfolioDisplay = document.querySelector(".portfolio-display");
 console.log(portfolioDisplay);
 
-// fetch("../data/data.json")
-//     .then((response) => response.json())
-//     .then((data) => {
-//         let production = data.production;
-//         console.log(production)
-
-
-
-//     });
-
-
-// fetch("../data/data.json")
-//     .then((response) => response.json())
-//     .then((data) =>
-//         data.map((e) => {
-//             portfolioDisplay.innerHTML += `<div class="col-lg-6 col-md-6 mix certified used ${e.tag}" style="display: inline-block;" data-bound="">
-//             <div class="single-portfolio">
-//                 <div class="portfolio-image bg-1">
-//                     <div class="price-wrap">
-//                         <a href="https://www.youtube.com/watch?v=dkTYpP6eec0" class="popup-youtube">
-//                             <i class="flaticon-play"></i>
-//                         </a>
-//                     </div>
-//                 </div>
-
-//                 <div class="portfolio-content">
-//                     <div>
-//                         <span>${e.tag}</span>
-
-//                         <h3>
-//                             <a href="portfolio-details.html">
-//                                 Fighting club
-//                             </a>
-//                         </h3>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-// `;
-//         })
-//     );
-
 fetch("../data/data.json")
     .then((response) => response.json())
     .then((data) => {
@@ -52,7 +10,7 @@ fetch("../data/data.json")
             <div class="single-portfolio">
                 <div class="portfolio-image bg-1">
                     <div class="price-wrap">
-                        <a href="${production[i].vimeoF}" >
+                        <a href="${production[i].vimeo}" >
                             <i class="flaticon-play"></i>
                         </a>
                     </div>
@@ -72,9 +30,6 @@ fetch("../data/data.json")
             </div>
         </div>`
         }
-
-
-
     });
 
 
