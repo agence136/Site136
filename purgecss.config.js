e286f926a38803ea64033b119be1ef6500410181
agence136 <elisabeth.malek@agence136.fr>
2023-08-23 18:45:25 +0200

    Amélioration performances site

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    content: ['*.html'],
-    css: ['./assets/css/magnific-popup.min.css'],
-  
-    output:'./assets/css/newCss',
+    content: ['index.html', './assets/html/404.html', './assets/html/agence.html', './assets/html/blog-details.html', './assets/html/blog.html', './assets/html/contact.html', './assets/html/offre.html', './assets/html/production-details.html', './assets/html/production.html'],
+    css: ['./assets/css/responsive.css'],
+    keyframes: true,
+    output: './assets/css/newCss',
     rejected: true,
-    variables:true,
-  }
+    variables: true,
+}
