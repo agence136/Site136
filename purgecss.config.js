b203d4f2af6a37eecaa9cf3a92e2c2a34cc8efb0
agence136 <elisabeth.malek@agence136.fr>
2023-08-23 17:01:40 +0200

    Purge du fichier css bootstrap css

---
+++ purgecss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['*.html'],
+    css: ['./assets/css/bootstrap.min.css'],
+    keyframes:true,
+    output:'./assets/css/newCss',
+    rejected: true,
+    variables:true,
+  }
