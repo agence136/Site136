3c725836903ca3d077698164ea67cb84f92fa09f
agence136 <elisabeth.malek@agence136.fr>
2023-08-24 12:15:25 +0200

    fix bug css animate

--- purgecss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    content: ['index.html', './assets/html/404.html', './assets/html/agence.html', './assets/html/blog-details.html', './assets/html/blog.html', './assets/html/contact.html', './assets/html/offre.html', './assets/html/production-details.html', './assets/html/production.html'],
-    css: ['./assets/css/responsive.css'],
-    keyframes: true,
-    output: './assets/css/newCss',
-    rejected: true,
-    variables: true,
-}
