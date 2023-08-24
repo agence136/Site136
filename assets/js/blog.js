9d0c6a8fd6300d9abde9121c42ac8b8ad4688543
agence136 <elisabeth.malek@agence136.fr>
2023-08-24 18:00:17 +0200

    Ajustement de la partie blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,12 +1,12 @@
 let blogDisplay = document.querySelector(".blog-display");
 console.log(blogDisplay);
 
-// Afficher l'ensemble des articles
+// Afficher l'ensemble des articles dans la page blog
 fetch("../data/data.json")
   .then((response) => response.json())
   .then((data) => {
     let blog = data.blog;
-    console.log(blog)
+    console.log(blog);
     for (let i in blog) {
       blogDisplay.innerHTML += `<div class="col-lg-6 col-md-6">
 <div class="single-blog">
@@ -33,22 +33,23 @@ fetch("../data/data.json")
 
     <p>${blog[i].extrait}</p>
 
-    <a href="blog-details.html" class="read-more">
-      Lire la suite...
-    </a>
+    <a href="blog-details.html?id=${blog[i].id}">
+    Lire la suite...
+</a>
   </div>
 </div>
-</div>`
+</div>`;
     }
-  })
+  });
 
+// Afficher les derniers articles sur la page d'accueil
 let blogIndex = document.querySelector(".blog-index");
 
 fetch("assets/data/data.json")
   .then((response) => response.json())
   .then((data) => {
     let latest = data.latest;
-    console.log(latest)
+    console.log(latest);
     for (let i in latest) {
       blogIndex.innerHTML += `<div class="col-lg-6 col-md-6">
 <div class="single-blog">
@@ -75,59 +76,64 @@ fetch("assets/data/data.json")
 
     <p>${latest[i].extrait}</p>
 
-    <a href="blog-details.html" class="read-more">
-      Lire la suite...
-    </a>
+    
+    <a href="assets/html/blog-details.html?id=${latest[i].id}">
+    Lire la suite...
+</a>
   </div>
 </div>
-</div>`
+</div>`;
     }
-  })
+  });
+
+// Afficher le détail des articles sur la page blog-details
+
+let blogDetail = document.querySelector(".blog-details-wrap");
+fetch("../data/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let blog = data.blog;
+    let params = new URLSearchParams(document.location.search);
+    let id = params.get("id");
+    console.log(id);
+    for (let i in blog) {
+      let blogId = blog[i].id;
+      if (blogId == id) {
+        blogDetail.innerHTML += `  <div class="blog-top-content-wrap">
+  <img
+    src="${blog[i].img}"
+    alt="Image"
+  />
+
+  <ul class="post-details">
+    <li>
+      <i class="bx bx-user"></i>
+      By Admin
+    </li>
+    <li>
+      <i class="bx bx-calendar"></i>
+      06/21/2020
+    </li>
+
+  </ul>
+
+  <h3>${blog[i].title}</h3>
+
+  <p>
+  ${blog[i].article}
+  </p>
 
 
 
+  <blockquote>
+    <i class="fa-solid fa-quote-left"></i>
+    <p>
+    ${blog[i].quote}
+    </p>
+  </blockquote>
 
 
-
-//     <div class="col-lg-6 col-md-6">
-//     <div class="single-blog">
-//       <a href="blog-details.html">
-//         <img src="assets/img/blog/blog1.png" alt="Image" />
-//       </a>
-
-//       <div class="blog-content">
-//         <ul>
-//           <li>
-//             <i class="flaticon-user"></i>
-//             <a href="#">Admin</a>
-//           </li>
-
-//           <li>
-//             <i class="flaticon-calendar"></i>
-//             17/07/2020
-//           </li>
-//         </ul>
-
-//         <a href="blog-details.html">
-//           <h3>Web TV Mazars : Un premier prix pour 136 !</h3>
-//         </a>
-
-//         <p>
-//           Il y a quelques jours, nous recevions le COM-ENT d’argent du
-//           prix de la communication disruptive / impertinente pour
-//           TheNextCFO, le web TV que nous avons pensée et produite pour
-//           le Groupe Mazars.
-//         </p>
-
-//         <a href="blog-details.html" class="read-more">
-//           Lire la suite
-//         </a>
-//       </div>
-//     </div>
-//   </div>
-
-
-
-
-
-
+</div>  `;
+      }
+    }
+  });
