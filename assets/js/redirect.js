a0cff54b8f89a4514eb266b78e8cd5bfd87f5899
agence136 <elisabeth.malek@agence136.fr>
2023-12-05 18:05:14 +0100

    ajout des liens vers la page offre

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,5 +1,6 @@
 let linkCom = document.querySelector(".redirect-com");
 
+
 linkCom.addEventListener("click", function () {
     console.log("clique");
     // Redirigez vers la page-de-redirection.html lorsque le bouton est cliqué
@@ -17,3 +18,11 @@ linkVal.addEventListener("click", function () {
     // Redirigez vers la page-de-redirection.html lorsque le bouton est cliqué
     window.location.href = "./assets/html/valeur.html";
 });
+
+let linkOffer = document.querySelectorAll(".offer-link");
+
+for (let i in linkOffer) (
+    linkOffer[i].addEventListener("click", function () {
+        window.location.href = "./assets/html/offre.html"
+    })
+);
